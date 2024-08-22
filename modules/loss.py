21494cfab36053ff04fff82d373b302acf941af7
cordutie <egutierreo@uc.cl>
2024-08-22 16:22:02 +0200

    trainers added.

--- modules/loss.py
+++ modules/loss.py
@@ -1,7 +1,10 @@
 import torch
 
+# Multiscale spectrogram loss --------------------------------------------------
+
+
 # MULTISCALE SPECTOGRAM HERE
-def multiscale_fft(signal, scales=[4096, 2048, 1024, 512, 256, 128], overlap=.75):
+def multiscale_fft(signal, scales=[256, 128, 64], overlap=.75):
     stfts = []
     for s in scales:
         S = torch.stft(
@@ -30,9 +33,133 @@ def multiscale_spectrogram_loss(x, x_hat):
 def safe_log(x):
     return torch.log(x + 1e-7)
 
-def loss_function(x, x_hat, mean, var):
-    reproduction_loss = multiscale_spectrogram_loss(x, x_hat)
-    KLD = - 0.5 * torch.sum(1+ safe_log(var) - mean.pow(2) - var)
-    # print("Reproduction Loss: ", reproduction_loss)
-    # print("KLD: ", KLD)
-    return reproduction_loss + KLD
+# Statistics loss ------------------------------------------------------------------
+
+from modules.seeds import *
+import torchaudio
+
+def correlation_coefficient(tensor1, tensor2):
+    tensor1_mean = torch.mean(tensor1)
+    tensor2_mean = torch.mean(tensor2)
+    tensor1_std = torch.std(tensor1)
+    tensor2_std = torch.std(tensor2)
+    
+    standardized_tensor1 = (tensor1 - tensor1_mean) / tensor1_std
+    standardized_tensor2 = (tensor2 - tensor2_mean) / tensor2_std
+    
+    correlation = torch.mean(standardized_tensor1 * standardized_tensor2)
+    
+    return correlation
+
+def statistics(signal, N_filter_bank, sample_rate, erb_bank, log_bank):
+    device = signal.device  # Get the device of the input signal tensor
+    size = signal.shape[0]
+
+    #low_lim = 20  # Low limit of filter
+    #high_lim = sample_rate / 2  # Centre freq. of highest filter
+    #
+    ## Initialize filter bank
+    #erb_bank = fb.EqualRectangularBandwidth(size, sample_rate, N_filter_bank, low_lim, high_lim)
+    #
+    ## Generate subbands for noise
+    #erb_bank.generate_subbands(signal)
+    # 
+    ## Extract subbands
+    #erb_subbands_signal = erb_bank.subbands[:, 1:-1]
+    erb_subbands_signal = erb_bank.generate_subbands(signal)[:, 1:-1]
+    
+    # Extract envelopes
+    env_subbands = torch.abs(hilbert(erb_subbands_signal))
+    
+    new_sample_rate = sample_rate // 4
+    downsampler = torchaudio.transforms.Resample(sample_rate, new_sample_rate).to(device)  # Move downsampler to device
+    
+    # Downsampling before computing 
+    envelopes_downsampled = []
+    for i in range(N_filter_bank):
+        envelope = env_subbands[:, i].float().to(device)  # Ensure the envelope is on the same device
+        envelopes_downsampled.append(downsampler(envelope).to(torch.float64))
+
+    subenvelopes = []
+    new_size = envelopes_downsampled[0].shape[0]
+
+    for i in range(N_filter_bank):
+        signal = envelopes_downsampled[i]
+        
+        ## Initialize filter bank
+        #log_bank = fb.Logarithmic(new_size, sample_rate, 6, 10, new_sample_rate // 4)
+        #
+        ## Generate subbands for noise
+        #log_bank.generate_subbands(signal)
+    
+        # Extract subbands
+        subenvelopes.append(log_bank.generate_subbands(signal)[:, 1:-1])
+    
+    # Extract statistics up to order 4 and correlations
+    statistics_1 = torch.zeros(N_filter_bank, 4, device=device)
+    for i in range(N_filter_bank):
+        mu = torch.mean(env_subbands[:, i])
+        sigma = torch.sqrt(torch.mean((env_subbands[:, i] - mu) ** 2))
+        statistics_1[i, 0] = mu * 100
+        statistics_1[i, 1] = sigma ** 2 / mu ** 2
+        statistics_1[i, 2] = (torch.mean((env_subbands[:, i] - mu) ** 3) / sigma ** 3) / 50
+        statistics_1[i, 3] = (torch.mean((env_subbands[:, i] - mu) ** 4) / sigma ** 4) / 500
+
+    statistics_2 = torch.zeros(N_filter_bank * (N_filter_bank - 1) // 2, device=device)
+    index = 0
+    for i in range(N_filter_bank):
+        for j in range(i + 1, N_filter_bank):
+            statistics_2[index] = correlation_coefficient(env_subbands[:, i], env_subbands[:, j])
+            index += 1
+
+    statistics_3 = torch.zeros(N_filter_bank * 6, device=device)
+    for i in range(N_filter_bank):
+        sigma_i = torch.std(envelopes_downsampled[i])
+        for j in range(6):
+            statistics_3[6 * i + j] = torch.std(subenvelopes[i][:, j]) / sigma_i
+
+    statistics_4 = torch.zeros(15, N_filter_bank, device=device)
+    for i in range(N_filter_bank):
+        counter = 0
+        for j in range(6):
+            for k in range(j + 1, 6):
+                statistics_4[counter, i] = correlation_coefficient(subenvelopes[i][:, j], subenvelopes[i][:, k])
+                counter += 1
+
+    statistics_5 = torch.zeros(6, N_filter_bank * (N_filter_bank - 1) // 2, device=device)
+    for i in range(6):
+        counter = 0
+        for j in range(N_filter_bank):
+            for k in range(j + 1, N_filter_bank):
+                statistics_5[i, counter] = correlation_coefficient(subenvelopes[j][:, i], subenvelopes[k][:, i])
+                counter += 1
+
+    return [statistics_1, statistics_2, statistics_3, statistics_4, statistics_5]
+
+def statistics_loss(original_signal, reconstructed_signal, N_filter_bank, sample_rate, erb_bank, log_bank, alpha=[10,50,100,100,100]):
+    original_statistics      = statistics(original_signal,      N_filter_bank, sample_rate, erb_bank, log_bank)
+    reconstructed_statistics = statistics(reconstructed_signal, N_filter_bank, sample_rate, erb_bank, log_bank)
+    
+    loss = []
+    for i in range(5):
+        loss_i = torch.sqrt(torch.sum((original_statistics[i] - reconstructed_statistics[i])**2))
+        # normalize depending on the amount of data (compute data from shape)
+        loss_i = loss_i / original_statistics[i].numel()
+        loss.append(loss_i)
+
+    #dot product between lists loss and alpha (ensure equal dtype)
+    final_loss = torch.dot(torch.tensor(loss, dtype=loss[0].dtype), torch.tensor(alpha, dtype=loss[0].dtype))
+    return 2**(final_loss/3-1)
+
+def batch_statistics_loss(original_signals, reconstructed_signals, N_filter_bank, sample_rate, erb_bank, log_bank):
+    batch_size = original_signals.size(0)
+    total_loss = 0.0
+
+    for i in range(batch_size):
+        original_signal = original_signals[i]
+        reconstructed_signal = reconstructed_signals[i]
+        loss = statistics_loss(original_signal, reconstructed_signal, N_filter_bank, sample_rate, erb_bank, log_bank)
+        total_loss += loss
+
+    average_loss = total_loss / batch_size
+    return average_loss
--- modules/trainer.py
+++ modules/trainer.py
@@ -3,8 +3,8 @@ from modules.loss import *
 from modules.architecture import *
 import matplotlib.pyplot as plt
 
-# Updated Trainer
-def train(model, optimizer, num_epochs, device, dataloader, save_dir):
+# trainer multiscale loss ------------------------------------------------------------------
+def train_multiscale_loss(model, optimizer, num_epochs, device, dataloader, save_dir):
     best_loss = float('inf')
     loss_history = []
 
@@ -13,8 +13,7 @@ def train(model, optimizer, num_epochs, device, dataloader, save_dir):
         overall_loss = 0
         
         for batch in dataloader:
-            x = batch
-            x = x.to(device)
+            x = batch.to(device)
 
             # Zero the parameter gradients
             optimizer.zero_grad()
@@ -23,7 +22,9 @@ def train(model, optimizer, num_epochs, device, dataloader, save_dir):
             x_hat, mean, var = model(x)
             
             # Compute main loss
-            loss = loss_function(x, x_hat, mean, var)
+            loss_1 = multiscale_spectrogram_loss(x, x_hat)
+            loss_2 = - 0.5 * torch.sum(1+ safe_log(var) - mean.pow(2) - var) # KLD
+            loss = loss_1 + loss_2
             overall_loss += loss.item()
 
             # Backward pass and optimization
@@ -43,15 +44,74 @@ def train(model, optimizer, num_epochs, device, dataloader, save_dir):
         # Save the latest model
         save_model(model, f"{save_dir}/last_model.pth")
 
-        # Plot the loss history
-        plt.figure()
-        plt.plot(range(1, len(loss_history) + 1), loss_history, marker='o', label='Training Loss')
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.title('Loss over Epochs')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig(f"{save_dir}/loss_plot.png")
-        plt.close()  # Close the figure to free up memory
+    # After all epochs, plot the loss history
+    plt.figure()
+    plt.plot(range(1, len(loss_history) + 1), loss_history, marker='o', label='Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss over Epochs')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"{save_dir}/loss_plot.png")
+    plt.show()  # Show the plot only once at the end
+
+    print(f"Training complete. Best model saved to {save_dir}/best_model.pth and the latest model saved to {save_dir}/last_model.pth")
+
+# Trainer statistics loss ------------------------------------------------------------------
+def train_statistics_loss(model, optimizer, num_epochs, device, dataloader, save_dir, N_filter_bank, frame_size, sample_rate):
+    erb_bank = fb.EqualRectangularBandwidth(frame_size, sample_rate, N_filter_bank, 20, sample_rate // 2)
+    new_size = frame_size // 4
+    new_sample_rate = sample_rate // 4
+    log_bank = fb.Logarithmic(new_size, new_sample_rate, 6, 20, new_sample_rate // 2)
+    
+    best_loss = float('inf')
+    loss_history = []
+
+    for epoch in range(num_epochs):
+        model.train()
+        overall_loss = 0
+        
+        for batch in dataloader:
+            x = batch.to(device)
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Forward pass
+            x_hat, mean, var = model(x)
+            
+            # Compute main loss
+            loss_1 = batch_statistics_loss(x, x_hat, N_filter_bank, sample_rate, erb_bank, log_bank)
+            loss_2 = - 0.5 * torch.sum(1+ safe_log(var) - mean.pow(2) - var) # KLD
+            loss = loss_1 + loss_2
+            overall_loss += loss.item()
+
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+
+        # Calculate the average loss for this epoch
+        avg_loss = overall_loss / len(dataloader)
+        loss_history.append(avg_loss)
+        print(f"\tEpoch {epoch + 1} \tAverage Loss: {avg_loss}")
+
+        # Save the best model if this epoch's loss is the lowest so far
+        if avg_loss < best_loss:
+            best_loss = avg_loss
+            save_model(model, f"{save_dir}/best_model.pth")
+        
+        # Save the latest model
+        save_model(model, f"{save_dir}/last_model.pth")
+
+    # After all epochs, plot the loss history
+    plt.figure()
+    plt.plot(range(1, len(loss_history) + 1), loss_history, marker='o', label='Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss over Epochs')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"{save_dir}/loss_plot.png")
+    plt.show()  # Show the plot only once at the end
 
     print(f"Training complete. Best model saved to {save_dir}/best_model.pth and the latest model saved to {save_dir}/last_model.pth")
